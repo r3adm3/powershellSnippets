2fe7e437b056032ffbfdfd9e157cdec637fceebf
r3adm3 <adrianjfarnell@gmail.com>
2024-12-12 11:47:39 +0000

    noted dependencies in script

--- get-groupOwners.ps1
+++ get-groupOwners.ps1
@@ -22,5 +22,8 @@ function getGroupInfo{
 
 $groupID = $argGroup #"8d04c529-76b1-4fb6-9fc0-8e3f28e1ae88"
 write-output ""
+write-output "Powershell Module needs to be installed, install-module -name microsoft.graph -scope CurrentUser"
+write-output "Don't forget to connect: connect-mggraph -scopes `"Group.Read.All`""
+
 getGroupInfo $groupID
 write-output ""
